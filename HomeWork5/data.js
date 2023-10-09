745316f66235b72a9c0f3c369be5d693a3ce158c
Aleksei <alex18w@mail.ru>
2023-10-09 23:46:26 +0500

    Практическое задание 5 (к занятию 10)

---
+++ HomeWork5/data.js
@@ -0,0 +1,8 @@
+const animalsInfo = `[
+{
+    "id": "1",
+    "name": "Puma Name",
+    "address": "https://w.forfun.com/fetch/e8/e8399e4f7002e6d35d4f309e5b2bb5c4.jpeg", 
+    "description": "nature animals"
+}
+]`;
